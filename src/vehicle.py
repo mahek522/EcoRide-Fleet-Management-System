4574c456a120d2a34879769b52172831a72aea84
Mahek Sultana <maheksultana152@gmail.com>
2026-01-10 15:39:01 +0530

    UC1: Created Vehicle class with basic attributes

---
+++ src/vehicle.py
@@ -0,0 +1,7 @@
+print("Welcome to Eco-Ride Urban Mobility System")
+
+class Vehicle:
+    def __init__(self, vehicle_id, model, battery_percentage):
+        self.vehicle_id = vehicle_id
+        self.model = model
+        self.battery_percentage = battery_percentage
