68e98290c761a54844cba20c2b8e521efdc2790c
Mahek Sultana <maheksultana152@gmail.com>
2026-01-10 15:42:54 +0530

    UC2: Added encapsulation with private attributes and validation

--- src/vehicle.py
+++ src/vehicle.py
@@ -4,4 +4,17 @@ class Vehicle:
     def __init__(self, vehicle_id, model, battery_percentage):
         self.vehicle_id = vehicle_id
         self.model = model
-        self.battery_percentage = battery_percentage
+        self.__maintenance_status = "Available"
+        self.set_battery_percentage(battery_percentage)
+
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+
+    def set_maintenance_status(self, status):
+        self.__maintenance_status = status
+
+    def set_battery_percentage(self, value):
+        if 0 <= value <= 100:
+            self.battery_percentage = value
+        else:
+            raise ValueError("Battery must be between 0 and 100")
