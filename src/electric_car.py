009a6dc8b5b19ef48b0fc432df98ac8571f5a975
Mahek Sultana <maheksultana152@gmail.com>
2026-01-11 14:53:06 +0530

    UC5: Implemented polymorphic fare calculation

--- src/electric_car.py
+++ src/electric_car.py
@@ -4,3 +4,6 @@ class ElectricCar(Vehicle):
     def __init__(self, vehicle_id, model, battery_percentage, seating_capacity):
         super().__init__(vehicle_id, model, battery_percentage)
         self.seating_capacity = seating_capacity
+        
+    def calculate_trip_cost(self, distance):
+        return 5 + (0.5 * distance)
